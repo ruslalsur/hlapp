f21911205d07600d35f65ad1b66aaca03fc76273
rusla <suhrugen@gmail.com>
2020-08-25 03:24:04 +0700

    удалил три огромных лога с трейсами, не лезло в гитхаб, он сказал чуть слышно - реджектед (три раза)...

--- public/index.php
+++ public/index.php
@@ -1,10 +1,57 @@
-<h1>Тестовый скрипт</h1>
+<h1>Логирование</h1>
 
 <?php
-  $start = microtime(true);
-  for ($x=1; $x<10000; $x++) {
-    for ($y=1; $y<5000; $y++) {
-      $x*$y-$x/$y+4596;
+require_once('../vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+define("DEBUG", true);
+
+if (DEBUG) {
+// создание экземпляра класса
+    $log = new Logger('log');
+    $log->pushHandler(new StreamHandler('../log/warning.log', Logger::WARNING));
+    $log->pushHandler(new StreamHandler('../log/debug.log', Logger::DEBUG));
+    $log->pushHandler(new StreamHandler('../log/info.log', Logger::INFO));
+    $log->pushHandler(new StreamHandler('../log/notice.log', Logger::NOTICE));
+    $log->pushHandler(new StreamHandler('../log/error.log', Logger::ERROR));
+    $log->pushHandler(new StreamHandler('../log/alert.log', Logger::ALERT));
+    $log->pushHandler(new StreamHandler('../log/critical.log', Logger::CRITICAL));
+    $log->pushHandler(new StreamHandler('../log/emergency.log', Logger::EMERGENCY));
+
+    // начало замера использования времени и памяти
+    $start = microtime(true);
+    $memory1 = memory_get_usage();
+}
+
+// тянем время создавая иллюзию бурной деятельности
+$data = array();
+for ($x = 1; $x < 4000; $x++) {
+    for ($y = 1; $y < 5000; $y++) {
+        $z = $x * $y - $x / $y + 4596;
     }
-  }
-  echo 'Время генерации: ' . ( microtime(true) - $start ) . ' сек.';
+    array_push($data, $z);
+}
+trigger_error('принудительный вызов ошибки для старта трассировки');
+
+if (DEBUG) {
+    // создание логов различных уровней
+    $log->warning('ПРЕДУПРЕЖДЕНИЕ! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->debug('ОТЛАДКА! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->info('ИНФОРМАЦИЯ! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->error('ОШИБКА! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->alert('ОПОВЕШЕНИЕ! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->critical('КРИТИЧНО! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+    $log->emergency('ЭКСТРЕННО! Время генерации: ' . (microtime(true) - $start) . ' сек.');
+
+    // изменения в использовании памяти
+    $memory2 = memory_get_usage();
+    $memory_diff = $memory2 - $memory1;
+    $log->debug('Изменения в использовании памяти: ' . $memory_diff . ' байтов');
+
+    echo 'Информация по логированию содержится в каталоге ../log';
+}
--- servers_up/mariadb/createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS hlappdb;
-CREATE USER 'hlappuser'@'localhost' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON appdb.* to 'hlappuser'@'localhost';
-
-FLUSH PRIVILEGES;
-
-SHOW DATABASES;
-SHOW GRANTS FOR hlappuser@localhost;
